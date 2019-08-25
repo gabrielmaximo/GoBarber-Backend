00b1454cca966cdb644d69e05491c6a87495291a
gabrielmaximo <familia_maximo@hotmail.com>
2019-08-25 18:35:48 -0300

    ERRO: configurando eslint, prettier, editorConfig

--- modules/module02/src/app.js
+++ modules/module02/src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -9,12 +9,13 @@ class App {
   }
 
   middlewares() {
-    this.server.use(express.json());
+    this.server.use(express.json())
   }
 
   routes() {
-    this.server.use(routes);
+    this.server.use(routes)
   }
 }
 
 export default new App().server;
+
